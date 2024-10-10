0ea9837c62c23c6d226b81ebd3963506247af880
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-10-10 16:05:16 +0200

    simpleDatatables.DataTable use tableOptions

--- assets/js/modules/simple-datatables/simple-datatables.load.js
+++ assets/js/modules/simple-datatables/simple-datatables.load.js
@@ -1,53 +1,24 @@
 // Adapted from https://github.com/fiduswriter/simple-datatables/blob/main/docs/demos/19-bootstrap-table/index.html
-
-
 let tableOptions = {
-    perPage: 20,
-    perPageSelect: [10, 20, 50, ["{{ T "tablePagingAll" }}", -1]],
+    perPage: 10,
     locale: "{{ site.Language.Lang | default "en" }}",
     labels: {
-      placeholder: "{{ T "tablePlaceholder" }}",
-      searchTitle: "{{ T "tablesSearchTitle" }}",
-      perPage: "{{ T "tablesPerPage" }}",
-      noRows: "{{ T "tablesNoRows" }}",
-      noResults: "{{ T "tablesNoResults" }}",
-      info: "{{ T "tablesInfo" }}"
+      "placeholder": "{{ T "tablePlaceholder" }}",
+      "searchTitle": "{{ T "tablesSearchTitle" }}",
+      "perPage": "{{ T "tablesPerPage" }}",
+      "noRows": "{{ T "tablesNoRows" }}",
+      "noResults": "{{ T "tablesNoResults" }}",
+      "info": "{{ T "tablesInfo" }}"
     },
-    // perPageSelect: [5, 10, 15, ["All", -1]],
-    classes: {
-        active: "active",
-        disabled: "disabled",
-        selector: "form-select",
-        paginationList: "pagination",
-        paginationListItem: "page-item",
-        paginationListItemLink: "page-link"
-    },
-    template: options => `<div class='${options.classes.top} fixed-table-toolbar'>
-    ${
-    options.paging && options.perPageSelect ?
-    `<div class='${options.classes.dropdown} bs-bars float-left'>
-    <label>
-    <select class='${options.classes.selector}'></select>
-    </label>
-    </div>` :
-    ""
-    }
-    ${
-    options.searchable ?
-    `<div class='${options.classes.search} float-right search btn-group'>
-    <input class='${options.classes.input} form-control search-input' placeholder='Search' type='search' title='Search within table'>
-    </div>` :
-    ""
-    }
-    </div>
-    <div class='${options.classes.bottom}'>
-    ${
-    options.paging ?
-    `<div class='${options.classes.info}'></div>` :
-    ""
-    }
-    <nav class='${options.classes.pagination}'></nav>
-    </div>`,
+    perPageSelect: [5, 10, 20, 50, ["{{ T "tablePageSelectAll" }}", -1]],
+     classes: {
+         active: "active",
+         disabled: "disabled",
+        // not used in hinode yet - no arrow    selector: "form-select",
+         paginationList: "pagination",
+         paginationListItem: "page-item",
+         paginationListItemLink: "page-link"
+     },    
     tableRender: (_data, table, _type) => {
         // ignore type ('main', 'print', 'header', 'message')
         const thead = table.childNodes[0]
@@ -79,13 +50,18 @@ let tableOptions = {
 
 document.querySelectorAll('.data-table').forEach(tbl => {
     let sortable = (tbl.getAttribute('data-table-sortable') === 'true')
+    tableOptions.sortable = sortable;
     let paging = (tbl.getAttribute('data-table-paging') === 'true')
+    tableOptions.paging = paging;
     let searchable = (tbl.getAttribute('data-table-searchable') === 'true')
+    tableOptions.searchable = searchable;
+    
+    // get localizations from hinode table shortcode
+    tableOptions.locale =  (tbl.getAttribute('data-table-locale'))
+    var labels = tbl.getAttribute('data-table-labels');
+        tableOptions.labels =  JSON.parse(labels)
 
-    new window.simpleDatatables.DataTable(tbl, {
-        sortable: sortable,
-        paging: paging, 
-        searchable: searchable,
+    new window.simpleDatatables.DataTable(tbl, 
         tableOptions
-    })
+    )
 })
